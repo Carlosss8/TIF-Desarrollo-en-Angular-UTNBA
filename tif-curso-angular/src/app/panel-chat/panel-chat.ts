cd07f0333e6a0446c75cb97a584f8ab4ede3b141
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-03-17 19:35:05 -0300

    update "panel-chat" new fuctions

--- tif-curso-angular/src/app/panel-chat/panel-chat.ts
+++ tif-curso-angular/src/app/panel-chat/panel-chat.ts
@@ -2,12 +2,25 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { Usuarios } from '../usuarios/usuarios';
+import { Usuario } from '../usuarios/usuarios';
+import { User } from '../services/user';
+import { Pipe } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-panel-chat',
-  imports: [MatSidenavModule, MatToolbarModule, MatListModule],
+  imports: [MatSidenavModule, MatToolbarModule, MatListModule, CommonModule],
   templateUrl: './panel-chat.html',
   styleUrl: './panel-chat.css',
 })
-export class PanelChat { }
+export class PanelChat {
+  usuarios: Usuario[] = []
+
+  constructor(private usuariosService: User) { }
+
+  ngOnInit(): void {
+    this.usuarios = this.usuariosService.obtenerUsuarios();
+  }
+}
