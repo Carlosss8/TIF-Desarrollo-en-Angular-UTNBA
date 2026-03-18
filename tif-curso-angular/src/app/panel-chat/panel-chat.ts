b6edc76764df4f766931eff9b6cb1f727ae226e2
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-03-18 18:24:47 -0300

    update panel-chat, new fuctions.

--- tif-curso-angular/src/app/panel-chat/panel-chat.ts
+++ tif-curso-angular/src/app/panel-chat/panel-chat.ts
@@ -1,26 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import { Usuarios } from '../usuarios/usuarios';
+import { Conversacion, Mensaje, Usuarios } from '../usuarios/usuarios';
 import { Usuario } from '../usuarios/usuarios';
 import { User } from '../services/user';
-import { Pipe } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms"
 
 
 @Component({
   selector: 'app-panel-chat',
-  imports: [MatSidenavModule, MatToolbarModule, MatListModule, CommonModule],
+  standalone: true,
+  imports: [MatSidenavModule, MatToolbarModule, MatListModule, CommonModule, ReactiveFormsModule],
   templateUrl: './panel-chat.html',
-  styleUrl: './panel-chat.css',
+  styleUrl: './panel-chat.css'
 })
-export class PanelChat {
+export class PanelChat implements OnInit {
   usuarios: Usuario[] = []
+  usuarioSeleccionado: Usuario | null = null;
+  conversaciones: Conversacion[] = []
+  conversacionActual!: Conversacion
+  form!: FormGroup
 
-  constructor(private usuariosService: User) { }
+  constructor(private usuariosService: User, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.usuarios = this.usuariosService.obtenerUsuarios();
+
+    this.form = this.fb.group({
+      mensaje: ['', [Validators.required, Validators.minLength(1)]]
+    })
   }
+
+  seleccionarUsuario(usuario: Usuario): void {
+    this.usuarioSeleccionado = usuario;
+
+    const conversacionEncontrada = this.conversaciones.find(c => c.numeroCel === usuario.numeroCel)
+
+    if (conversacionEncontrada) {
+      this.conversacionActual = conversacionEncontrada
+    } else {
+      const nuevaConversacion: Conversacion = {
+        numeroCel: usuario.numeroCel,
+        mensajes: []
+      }
+      this.conversaciones.push(nuevaConversacion)
+      this.conversacionActual = nuevaConversacion
+    }
+  }
+
+  enviarMensaje() {
+    if (this.form.valid) {
+      const texto = this.form.value.mensaje
+      const nuevoMensaje: Mensaje = {
+        texto: texto,
+        emisor: 'usuario',
+        hora: new Date().toLocaleTimeString()
+      }
+      this.conversacionActual.mensajes.push(nuevoMensaje)
+      this.form.reset()
+
+      setTimeout(() => {
+        const respuestaApp: Mensaje = {
+          texto: 'Hola, recibí tu mensaje',
+          emisor: 'app',
+          hora: new Date().toLocaleTimeString()
+        };
+
+        this.conversacionActual.mensajes.push(respuestaApp);
+      }, 1000);
+    }
+
+  }
+
+
+
 }
